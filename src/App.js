9616835fd71c3c2f063dfb85e8bc9b91db7c6fcc
Almog Wertzberger <Almogish@gmail.com>
2021-09-12 14:11:34 +0300

    adding classNames

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
   return (
     <section className="section">
       <div className="title">
@@ -15,9 +16,19 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
-          //later
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt="name" className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -26,10 +37,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
